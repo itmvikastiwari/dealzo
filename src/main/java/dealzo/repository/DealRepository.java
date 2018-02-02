b3d4ec302da2f14375ad91732c7dae8a64e51bb8
thevicky <vtiwari@quikr.com>
2018-02-02 18:41:59 +0530

    apis

--- src/main/java/dealzo/repository/DealRepository.java
+++ src/main/java/dealzo/repository/DealRepository.java
@@ -5,10 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface DealRepository extends MongoRepository<Deal, String>, QueryByExampleExecutor<Deal> {
 
-    List<Deal> findByUserId(int userId);
 }
